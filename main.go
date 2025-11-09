c2b2aaf35bc85a394818e4d964b8c25cd93374f1
Shamil Irfan <shamilirfan28@gmai.com>
2025-11-09 20:46:57 +0600

    .

--- main.go
+++ main.go
@@ -7,5 +7,3 @@ import (
 func main() {
 	cmd.Serve()
 }
-
-// :4000/reset-password-form?token=695ac6fe0ebaf8809caaf647243cd7b0e4f5091795a6fbca59c0b3b209c1d633:1  GET http://localhost:4000/reset-password-form?token=695ac6fe0ebaf8809caaf647243cd7b0e4f5091795a6fbca59c0b3b209c1d633 404 (Not Found)
