2f3206e6780b2a5cab4abcfdbe6d44dd50f4241a
Shamil Irfan <shamilirfan28@gmai.com>
2025-11-14 19:16:57 +0600

    .

--- main.go
+++ main.go
@@ -7,6 +7,3 @@ import (
 func main() {
 	cmd.Serve()
 }
-
-// আমি চাইলে পরবর্তী ধাপে search, filter, category-wise get + pagination সহ full featured GetBooks API বানিয়ে দিতে পারি।
-// চাও কি আমি সেটা করি?
--- rest/middlewares/cors_with_preflight.go
+++ rest/middlewares/cors_with_preflight.go
@@ -1,16 +1,25 @@
 package middlewares
 
 import (
+	"bookShop/config"
 	"net/http"
+	"strconv"
 )
 
 func CorsWithPreflight(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// Cors handling
-		w.Header().Set("Access-Control-Allow-Origin", "*") // * means any IP address can access
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+		/* Cors handling
+		Allowed origin = frontend origin
+		*/
+		str := strconv.Itoa(int(config.GetConfig().HttpPort))
+		w.Header().Set("Access-Control-Allow-Origin", "http://localhost:"+str)
+		w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+		// Allowed headers
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
-		w.Header().Set("Content-Type", "application/json")
+
+		// Allowed methods
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
 
 		// Handle preflight OPTIONS request
 		if r.Method == http.MethodOptions {
